9a07092a8b3c62a49d9dc083fa7135636b083018
Craig Riley <68274157+craigrileyuk@users.noreply.github.com>
2024-12-20 07:56:41 +0000

    Version bump

--- inertia-wordpress.php
+++ inertia-wordpress.php
@@ -3,7 +3,7 @@
 /*
  * Plugin Name: Inertia Wordpress
  * Description: Connect an Inertia frontend theme to your Wordpress application, based on Inertia Laravel 2.0.0
- * Version: 0.1.0
+ * Version: 0.2.0
  * Requires at least: 6.0
  * Requires PHP: 8.2
  * Tested up to: 6.7
