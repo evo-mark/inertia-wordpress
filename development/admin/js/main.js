35f2916ed21c00a302576bfe12365179ad4e4669
Craig Riley <c.riley@evomark.co.uk>
2025-05-14 23:45:04 +0100

    Change import source for unhead #patch

--- development/admin/js/main.js
+++ development/admin/js/main.js
@@ -2,15 +2,15 @@ import { createApp } from "vue";
 import { vuetify } from "./plugins";
 import router from "./router";
 import DefaultLayout from "./layouts/Default.vue";
-import { createHead } from "@unhead/vue";
+import { createHead } from "@unhead/vue/client";
 import { SnackbarService } from "vue3-snackbar";
 import "../css/style.postcss";
 
 const head = createHead();
 
 createApp(DefaultLayout)
-    .use(head)
-    .use(router)
-    .use(vuetify)
-    .use(SnackbarService)
-    .mount("#inertia-wordpress__admin-page--wrapper");
+	.use(head)
+	.use(router)
+	.use(vuetify)
+	.use(SnackbarService)
+	.mount("#inertia-wordpress__admin-page--wrapper");
